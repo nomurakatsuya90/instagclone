825908b26a7a7f1e05fe9da6b484ff70c4c67eec
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-17 05:11:51 +0000

    index画面のImage欄を画像に変更。画像サイズ調整。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick  #コメントを外した　k.nomura 0317
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  process :resize_to_limit => [500, 500] # 画像サイズの調整
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
