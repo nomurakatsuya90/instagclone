99dd317ae3a95904d6fddd9208ca190cdb6179df
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-18 05:47:44 +0000

    Favoriteモデル追加。favorites中間でのpictures対usersアソシエーション作成。rails cでの動確済。

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ module Fbclone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|  #追記 0318 k.nomura
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
