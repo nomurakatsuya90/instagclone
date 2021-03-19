7ae70305e59441f83805939f0fb1542473e190f7
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-19 10:47:02 +0000

    Userの画像アップロード実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,8 @@ class UsersController < ApplicationController
   # private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,
+                                 :image, :image_cache )
   end
   # def user_params_update
   #   params.require(:user).permit(:name, :email)
