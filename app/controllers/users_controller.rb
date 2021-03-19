03b4e091486e2500a37637d3748a55e173ba9d1f
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-19 08:42:19 +0000

    Userの編集画面・機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
-  
+  before_action :set_user, only: %i[ show edit update ]
+
   def new
     @user = User.new
   end
@@ -14,12 +15,29 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
-  private
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "userを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  # private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  # def user_params_update
+  #   params.require(:user).permit(:name, :email)
+  # end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
