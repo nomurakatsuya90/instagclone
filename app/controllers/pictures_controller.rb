05168ec3ba618e0cccb66238f2a1ece6afa46393
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-20 06:33:56 +0000

    privateより下の行のインデント調整。Gemfileの不要な空行削除。

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -67,15 +67,15 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def picture_params
-      params.require(:picture).permit(:image, :comment, :image_cache) # :user_id
-    end
+  # Only allow a list of trusted parameters through.
+  def picture_params
+    params.require(:picture).permit(:image, :comment, :image_cache) # :user_id
+  end
 
   def guard_action_not_current_user
     if current_user.id != @picture.user_id
